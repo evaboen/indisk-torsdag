12e52f05ea74cf39041317af2fc8678d9b13e0f4
Erlend Vaboen <erlendvaboen@Erlends-MacBook-Air.local>
2025-09-23 23:16:05 +0200

    more stuff

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -1,36 +1,40 @@
 // src/components/Auth.tsx
 
-import React, { useState } from 'react';
-import { signInWithGoogle } from '../firebase/auth';
-import { UserCredential } from 'firebase/auth';
+import React, { useState } from "react";
+import { signInWithGoogle } from "../firebase/auth";
+import { UserCredential } from "firebase/auth";
+import styled from "styled-components";
 
 interface IProps {
-    setUser: (user: UserCredential | undefined) => void
+  setUser: (user: UserCredential | undefined) => void;
 }
 
 const Auth = (props: IProps) => {
+  const handleGoogleSignIn = async () => {
+    try {
+      const user = await signInWithGoogle();
+      props.setUser(user);
+      sessionStorage.setItem("signedInUser", JSON.stringify(user));
 
+      console.log("User signed in with Google:", user);
+    } catch (error) {
+      console.error("Google sign in error:", error);
+    }
+  };
 
-    const handleGoogleSignIn = async () => {
-        try {
-            const user = await signInWithGoogle();
-            props.setUser(user)
-            sessionStorage.setItem("signedInUser", JSON.stringify(user));
-
-            console.log('User signed in with Google:', user);
-        } catch (error) {
-            console.error('Google sign in error:', error);
-        }
-    };
-
- 
-
-    return (
-        <div>
-            <h2>Authentication</h2>
-            <button onClick={handleGoogleSignIn}>Sign In with Google</button>
-        </div>
-    );
+  return (
+    <AuthCard>
+      <h2>Lobbyen</h2>
+      <button onClick={handleGoogleSignIn}>Logg inn på denne</button>
+    </AuthCard>
+  );
 };
 
 export default Auth;
+
+const AuthCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  background: rgb(169, 69, 169);
+`;
